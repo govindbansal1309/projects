c214d9efd7ccc98a65d20e4b99f082238f7b5134
govindbansal1309 <govindbansal.1309@gmail.com>
2021-03-18 09:03:50 +0530

    added function in helloworld and with it created launch and tasks json file

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
+int myfun(int z)
+{
+  //this function is added from windows. it returns square of z. 
+  return z*z;
+}
 int main()
 {
-  printf("Hello world");
+  printf("Hello world\n");
   // new updates from linux
   int x = 2, y = 3;
   int z = x+y;
-  printf("%d", z);
+  printf("\n%d", myfun(z));
   return 0;
 }
